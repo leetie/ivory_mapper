3ec2f89d113212fe7f12449e934914754954f520
Jesse Parsons <savvyjesse@aol.com>
2020-05-27 15:06:09 -0400

    add demons

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-window.onload = init;
+const locationText = document.getElementById('location-text')
+const defaultCoordinates = [ -7960511.450303138, 5341189.183472256 ]
+const selectButtons = document.querySelectorAll('.selectButton');
+var selectedRadius = 5
+window.onload = init, console.log("hi"), locationText.textContent = locationText.textContent + " " + defaultCoordinates
+
 
 function init() {
   const map = new ol.Map({
     view: new ol.View({
       center: [ -7960511.450303138, 5341189.183472256 ],
-      zoom: 10
-      //maxZoom, minZoom
+      zoom: 9
+      //maxZoom, minZoom, rotation
     }),
     layers: [
       new ol.layer.Tile({
@@ -17,5 +22,13 @@ function init() {
 
   map.on('click', function (e){
     console.log(e.coordinate);
+    locationText.textContent = e.coordinate
   })
 };
+
+selectButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    console.log(e.target.firstChild.data);
+    selectedRadius = Number(e.target.firstChild.data)
+  })
+});
